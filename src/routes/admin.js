1a0addaefe82af8463440cb4ad6b9f79da0d67be
Davide Pacco <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-03-23 19:54:39 +0100

    CSV Upload working

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,4 +1,11 @@
+const _ = require("lodash");
+const multer = require("multer");
+const upload = multer();
+
+const Course = require("../db").Course;
 const Class = require("../db").Class;
+const Exam = require("../db").Exam;
+
 const UsersFacade = require("../db/transactionalUsersFacade").Facade;
 
 const { authenticate, adminOnlyMiddleware } = require("../auth");
@@ -43,7 +50,10 @@ adminRouter.post(
 
 adminRouter.route("/register/:token").post(async (req, res) => {
   try {
-    let registeredAdmin = await UsersFacade.registerAdminWithToken(req.body, req.params.token);
+    let registeredAdmin = await UsersFacade.registerAdminWithToken(
+      req.body,
+      req.params.token
+    );
     res.status(201).send(registeredAdmin);
   } catch (error) {
     if (error.type && error.type === "ClientError") {
@@ -66,16 +76,55 @@ adminRouter.route("/register/:token").get(async (req, res) => {
   }
 });
 
-adminRouter.get("/registration/:id", authenticate, adminOnlyMiddleware, async (req, res) => {
-  try {
-    res.status(200).send({ registration_token: await UsersFacade.getRegistrationTokenForStudent(req.params.id) });
-  } catch (error) {
-    if (error.type && error.type === "ClientError") {
-      res.status(400).send(error.message);
-    } else {
-      res.status(500).send(error.message);
+adminRouter.get(
+  "/registration/:id",
+  authenticate,
+  adminOnlyMiddleware,
+  async (req, res) => {
+    try {
+      res.status(200).send({
+        registration_token: await UsersFacade.getRegistrationTokenForStudent(
+          req.params.id
+        ),
+      });
+    } catch (error) {
+      if (error.type && error.type === "ClientError") {
+        res.status(400).send(error.message);
+      } else {
+        res.status(500).send(error.message);
+      }
     }
   }
-});
+);
+
+adminRouter
+  .route("/fromCSV")
+  .post(upload.single("file"), async (req, res, next) => {
+    try {
+      const toJson = bufferToJson(req.file.buffer);
+      const imported = await Exam.bulkCreate(toJson);
+      res.status(200).send(imported);
+    } catch (e) {
+      console.log(e);
+      res.send(e.message);
+    }
+  });
+
+const bufferToJson = (bufferData) => {
+  const lines = String(bufferData).split("\n");
+  const headers = lines[0]
+    .split(",")
+    .map((str) => _.trim(str, "\r"))
+    .map((str) => _.trim(str, '"'));
+  return _.tail(lines).map((row) =>
+    _.zipObject(
+      headers,
+      row
+        .split(",")
+        .map((str) => _.trim(str, "\r"))
+        .map((str) => _.trim(str, '"'))
+    )
+  );
+};
 
 module.exports = adminRouter;
